9fae980f963e4974c08ab9eea4893f1e5cb7040f
yonyzarate <yony.zarate96@gmail.com>
2022-06-09 00:00:35 -0400

    link del menu de categoria

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -2,7 +2,11 @@
             <nav class="sidebar-nav">
                 <ul class="nav">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Dashbord</a>
+                        <a class="nav-link" href="{{url('home')}}" onclick="event.preventDefault();
+                        document.getElementById('home-form').submit();"><i class="fa fa-list"></i> Dashbord</a>
+                        <form id="home-form" action="{{url('home')}}" method="GET" style="display: none;">
+                        {{csrf_field()}}
+                        </form>
                     </li>
                     <li class="nav-title">
                         Menú
@@ -10,7 +14,11 @@
 
                    
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa fa-list"></i> Categorías</a>
+                        <a class="nav-link" href="{{url('categoria')}}" onclick="event.preventDefault();
+                        document.getElementById('categoria-form').submit();"><i class="fa fa-list"></i> Categorías</a>
+                        <form id="categoria-form" action="{{url('categoria')}}" method="GET" style="display: none;">
+                        {{csrf_field()}}
+                        </form>
                     </li>
                     
                     <li class="nav-item">
