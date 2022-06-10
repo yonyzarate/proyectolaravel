1a2e2773966e9c6b79c247d41496f556dfd0adbc
yonyzarate <yony.zarate96@gmail.com>
2022-06-10 02:12:50 -0400

    Producto
    registrar producto

--- resources/views/producto/form.blade.php
+++ resources/views/producto/form.blade.php
@@ -12,19 +12,21 @@
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="codigo">Codigo</label>
     <div class="col-md-9">
-    <input type="text" name="codigo" id="codigo" class="form-control" placeholder="Ingrese el codigo">
+    <input type="number" name="codigo" id="codigo" class="form-control" 
+    pattern="[0,9]{0,15}" required placeholder="Ingrese el codigo">
     </div>
 </div>
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="nombre">Nombre</label>
     <div class="col-md-9">
-    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Ingrese el nombre">
+    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Ingrese el nombre"
+    required pattern="^[a-zA-Z_áéíóúñ\s]{*,100}$">
     </div>
 </div>
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="precio_venta">Precio de venta</label>
     <div class="col-md-9">
-    <input type="text" name="precio_venta" id="precio_venta" class="form-control" placeholder="Ingrese el precio de venta">
+    <input type="number" name="precio_venta" id="precio_venta" class="form-control" placeholder="Ingrese el precio de venta">
     </div>
 </div>
 <div class="form-group row">
